909d360d2be36963bca5ece5e70ca2bf615900e1
Fahrizal Bin Fahruddin <fahrizal.fahruddin@gmail.com>
2024-05-06 19:28:13 +0800

    Add project files.

---
+++ ProductManagementAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementAPI.Entities;
+
+namespace ProductManagementAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    protected readonly IConfiguration _config;
+
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    }
+}
---
+++ ProductManagementAPI/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProductManagementAPI.Entities;
+
+namespace ProductManagementAPI.Repositories;
+
+public interface IProductRepository
+{
+    void AddProduct(Product product);
+    void DeleteProduct(int id);
+    List<Product> GetAllProducts();
+    Product GetProductById(int id);
+    void UpdateProduct(Product product);
+}
